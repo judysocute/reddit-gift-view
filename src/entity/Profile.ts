6ce6ec3fafccd96f33b89b721e7267f3d6815218
wayne <judysocute@gmail.com>
2019-12-08 14:12:30 +0800

    [entity] add Profile class

---
+++ src/entity/Profile.ts
@@ -0,0 +1,16 @@
+/**
+ * 公開資料
+ */
+export default class Profile {
+  /**
+   * reddit username
+   */
+  public redditUsername: string = '';
+  public shortBioHtml: string = '';
+  /**
+   * 大頭貼
+   */
+  public photoUrl: string = '';
+  public isElf: boolean = false;
+  public shortBioSnudown: string = '';
+}
