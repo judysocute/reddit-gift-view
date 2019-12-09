07f62c65c8c5916ea869b39c5cfdedfda34af14a
wayne <judysocute@gmail.com>
2019-12-09 20:40:56 +0800

    [router] update home page transform to gallery page

--- src/components/gift/Info.vue
+++ src/components/gift/Info.vue
@@ -27,7 +27,7 @@ export default class Info extends Vue {
    */
   public toGallery() {
     this.$router.push({
-      name: 'home',
+      name: 'gallery',
     });
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
+import Gallery from '@/views/Gallery.vue';
 import GiftPage from '@/components/gift/GiftPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home,
+    path: '/gallery',
+    name: 'gallery',
+    component: Gallery,
   },
   {
     path: '/gift/:slug',
