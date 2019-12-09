87d174d3245b1f96606835e7e0b9a603c25822b0
wayne <judysocute@gmail.com>
2019-12-09 20:40:03 +0800

    [layout] add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navbar from '@/components/Navbar.vue';
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {
+}
+</script>
+
 
 <style lang="scss">
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+    <div class="logo cursor-pointer" @click="toGallery">
+      <p class="logo-text">
+        <span class="reddit">reddit</span>
+        <span class="gift">gifts</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class Navbar extends Vue {
+  public toGallery() {
+    this.$router.push({
+      name: 'gallery',
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  margin-bottom: 10px;
+  display: flex;
+  border-bottom: 1px solid $light;
+  .logo {
+    padding: 25px;
+    .logo-text {
+      font-size: 25px;
+      margin: 0;
+      .reddit {
+        color: $logo-left-color;
+      }
+      .gift {
+        color: $deep-blue;
+      }
+    }
+  }
+}
+</style>
